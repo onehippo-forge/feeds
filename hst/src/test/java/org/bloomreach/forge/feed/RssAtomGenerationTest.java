d5aba9e9c77eb80ea55a3b5f8fde688dd1f9a590
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2020-02-06 17:42:50 +0100

    HIPFORGE-335 Feeds: upgrade to v14
    - move test class

--- hst/src/test/java/org/bloomreach/forge/feed/RssAtomGenerationTest.java
+++ hst/src/test/java/org/bloomreach/forge/feed/RssAtomGenerationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import java.io.StringWriter;
+package org.bloomreach.forge.feed;import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
